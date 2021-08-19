eb7ec102685986f7608b2026a15dd42317d1926c
terciotales <thoorgames18@gmail.com>
2021-08-19 16:42:22 -0300

    Cria pasta e adiciona 1001

---
+++ 1001 - 1050/1001.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+ 
+public class Main {
+ 
+   public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int a = leitor.nextInt();
+        int b = leitor.nextInt();
+        int x = a + b;
+        System.out.println("X = " + x);
+    }
+ 
+}
