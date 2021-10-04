b9efd9bc7dc6cb4135450d15138528403a8b573e
Tércio Tales Barnabé <thoorgames18@gmail.com>
2021-10-04 19:02:10 -0300

    Adiciona arquivos

--- ex1010.java
+++ ex1010.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
-
-import java.io.IOException;
-import java.util.Scanner;
 public class ex1010 {
-        public static void main(String[] args) throws IOException {
-            Scanner ler = new Scanner(System.in);
-             double R, pi, volume;
-             R = ler.nextDouble();
-             pi = 3.14159;
-             volume = (4 * pi * (R * R * R)) / 3;    
-             System.out.printf("VOLUME = %.3f\n", volume);
-    }
+    public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int qtd1, qtd2, cod1, cod2;
+		double valor1, valor2, valorFinal;
+		cod1 = scan.nextInt();
+		qtd1 = scan.nextInt();
+		valor1 = scan.nextDouble();
+		cod2 = scan.nextInt();
+		qtd2 = scan.nextInt();
+		valor2 = scan.nextDouble();
+		valorFinal = (qtd1 * valor1) + (qtd2 * valor2);
+		System.out.printf("VALOR A PAGAR: R$ %.2f\n", valorFinal);
+	}
 }
